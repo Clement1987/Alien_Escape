7998088551257219ed7cfdce11dd6f53b1245877
Quentin Liger <quentinlgr45160@gmail.com>
2019-10-12 01:37:24 +0200

    fix algo choice & attribut strength

--- AlienEscape.java
+++ AlienEscape.java
@@ -7,7 +7,7 @@ public class AlienEscape {
 
     private static Scanner sc = new Scanner(System.in);
     private static ArrayList<String> equipement = new ArrayList<>();
-    private static MyAlien alien = new MyAlien(name);
+    private static MyAlien alien = new MyAlien("name");
 
     public static void main (String[] args) {
         //Title début
@@ -18,7 +18,7 @@ public class AlienEscape {
     }
 
     public static void LaunchGame() {   
-        
+
 
     }
 
@@ -31,7 +31,6 @@ public class AlienEscape {
 
 
     //QUENTIN
-    
 
     //GLOBAL
 
@@ -40,17 +39,22 @@ public class AlienEscape {
     }
 
     public static String choice (String question, String[] choices) {
-        String[] answerIndex = new String [] {"1", "2", "3", "4", "5"};
+        String[] answerIndex = new String [choices.length];
+        for (int j = 0; j < choices.length; j++) {
+            answerIndex[j] = Integer.toString(j+1);
+        }
         String[] answerList = choices;
 		String playerAnswer = "";
-		while (!Arrays.asList(answerList).contains(playerAnswer)) {
+		while (!Arrays.asList(answerIndex).contains(playerAnswer)) {
             say(question);
             say("");
             for (int i = 0; i < answerList.length; i++) {
                 say( "< " + answerIndex[i] + " > " + answerList[i]);
             }
             say("");
+            System.out.print("Answer : ");
             playerAnswer = sc.nextLine();
+            say("");
         }
         return playerAnswer;
     }
