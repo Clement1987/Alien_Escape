af08b22bbeac305fa5fb63a2fba74be25839f25f
Quentin Liger <quentinlgr45160@gmail.com>
2019-10-12 00:59:27 +0200

    ajout equipement et méthode choice

--- AlienEscape.java
+++ AlienEscape.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AlienEscape {
 
     private static Scanner sc = new Scanner(System.in);
+    private static ArrayList<String> equipement = new ArrayList<>();
 
     public static void main (String[] args) {
         //Title début
@@ -14,8 +18,6 @@ public class AlienEscape {
 
     public static void LaunchGame() {   
 
-        stay();
-        firstMeet();
 
     }
 
@@ -34,6 +36,22 @@ public class AlienEscape {
 
     public static void say (String message) {
         System.out.println(message);
+    }
+
+    public static String choice (String question, String[] choices) {
+        String[] answerIndex = new String [] {"1", "2", "3", "4", "5"};
+        String[] answerList = choices;
+		String playerAnswer = "";
+		while (!Arrays.asList(answerList).contains(playerAnswer)) {
+            say(question);
+            say("");
+            for (int i = 0; i < answerList.length; i++) {
+                say( "< " + answerIndex[i] + " > " + answerList[i]);
+            }
+            say("");
+            playerAnswer = sc.nextLine();
+        }
+        return playerAnswer;
     }
 
 }
