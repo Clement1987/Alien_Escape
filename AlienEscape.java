90750620e831ac941cdd80fbc8262db1dea7d643
Quentin Liger <quentinlgr45160@gmail.com>
2019-10-12 02:03:37 +0200

    ajout status de jeu (playing)

--- AlienEscape.java
+++ AlienEscape.java
@@ -9,10 +9,12 @@ public class AlienEscape {
     private static ArrayList<String> equipement = new ArrayList<>();
     private static MyAlien alien = new MyAlien("name");
     private static String playerAnswer;
+    private static boolean playing;
 
     public static void main (String[] args) {
         //Title début
 
+        playing = true;
         LaunchGame();
 
         //Title fin
@@ -20,7 +22,7 @@ public class AlienEscape {
 
     public static void LaunchGame() {
 
-        stay();
+        if (playing) stay(); 
 
     }
 
